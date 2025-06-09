99e1ea33646de1bf40fa4dd7db5e2a21c59b79ba
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-09 16:08:12 +0900

    문자열

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {2, 4, 6, 8, 10};
-        int sum = 0;
+        String message = "Hello, Java!";
 
-        for (int i = 0; i < numbers.length; i++) {
-            sum += numbers[i];
+        System.out.println("문자열 길이: " + message.length());
+
+        System.out.println("대문자 변환: " + message.toUpperCase());
+        System.out.println("소문자 변환: " + message.toLowerCase());
+
+        System.out.println("J의 인덱스: " + message.indexOf("J"));
+        System.out.println("중복된 글자 인덱스: " + message.indexOf("l") + ", " + message.indexOf("a"));
+
+        for (int i = 0; i < message.length(); i ++) {
+            if(message.charAt(i) == 'a') {
+                System.out.println(i);
+            }
         }
-        System.out.println(sum);
+        for (int i = 1; i <= message.length(); i++) {
+            System.out.println(message.substring(i));
+
+        }
+
+        System.out.println(message.substring(3, 4));
+
+        String address = "0x120819f5573ac3A530e6172b1581613A09b4b338";
+
+        System.out.println(address.substring(0, 7).equals("0x12081"));
+
+        System.out.println(address.substring(address.length() - 5));
+        System.out.println(
+                address.substring(0, 7)
+                + "..."
+                + address.substring(address.length() - 5)
+        );
+
     }
 }
