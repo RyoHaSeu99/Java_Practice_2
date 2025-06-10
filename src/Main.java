587578f0e608ba92775d6b54212dbada8c087b10
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-10 10:28:37 +0900

    클래스, 객체

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,56 @@
+class Person {
+    String name;
+    int age; // 필드(클래싀 안의 변수)
+    String mbti;
+
+    public void sayHello() {
+        System.out.println("안녕하세요? 저는 " + name + "입니다. 저의 나이는 " + age + "살이고 mbti는 " + mbti + "입니다.");
+
+    }
+    public void work() {
+            System.out.println("일하는 중...");
+
+    }
+}
+
+class Car {
+    String brand;
+    int year;
+
+    public void displayinfo() {
+        System.out.println("브랜드: " + brand);
+        System.out.println("연식: " + year);
+    }
+}
+
+
+
 public class Main {
     public static void main(String[] args) {
-        int number = 123;
-        String binartString = Integer.toBinaryString(number);
-        String hexString = Integer.toHexString(number);
+        Person person = new Person();   // 객체(=인스턴스)
 
-        System.out.println("십진수: " + number);
-        System.out.println("이진수: " + binartString);
-        System.out.println("십육진수: " + hexString);
+        person.name = "한철규";
+        person.age = 27;
+        person.mbti = "istp";
+
+        person.sayHello();
+
+        person.work();
+
+        Person mzPerson = new Person();
+        mzPerson.name = "신태희";
+        mzPerson.age = 24;
+        mzPerson.mbti = "isfj";
+
+        mzPerson.sayHello();
+        mzPerson.work();
+
+        Car myCar = new Car();
+
+        myCar.brand = "현대";
+        myCar.year = 18;
+
+        myCar.displayinfo();
+
     }
 }
